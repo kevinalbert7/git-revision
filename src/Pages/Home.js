4bd08c49a5f131bd983b03d3e6e2e15f508439d8
benpaquier <hello@benpaquier.com>
2021-12-10 10:17:22 +0100

    Homepage

---
+++ src/Pages/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div>
+      bonjourbonjourewf
+      creerivher
+      ihvepvh
+      hovehi
+    </div>
+  );
+};
+
+export default Home;
